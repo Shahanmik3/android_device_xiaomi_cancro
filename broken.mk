047c8bd771e85381e806ec727f7f4438e5f3ff11
Shahan-mik3 <shahan.mike13@gmail.com>
2015-12-24 09:33:32 +0000

    Broken bringup

--- broken.mk
+++ broken.mk
@@ -1,5 +1,4 @@
-# Copyright (C) 2014 The CyanogenMod Project
-#
+# Copyright (C) 2014 The CyanogenMod Project#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -21,13 +20,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/cancro/cancro.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff (mini)
-$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
+$(call inherit-product, vendor/broken/config/common_mini_phone.mk)
 
 # Product configuration
-PRODUCT_NAME := cm_cancro
+PRODUCT_NAME := broken_cancro
 PRODUCT_DEVICE := cancro
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
@@ -39,7 +38,7 @@ TARGET_VENDOR_PRODUCT_NAME := cancro
 TARGET_VENDOR_DEVICE_NAME := MI3W
 
 # Unofficial build id
-TARGET_UNOFFICIAL_BUILD_ID := audahadi
+TARGET_UNOFFICIAL_BUILD_ID := Shahan_mik3
 
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=cancro PRODUCT_NAME=cancro
 
